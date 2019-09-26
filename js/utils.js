adc724baecf60e5303fea0a3baf4131b35e3ee46
liuning <liuning@gwmfc.com>
2019-09-26 09:01:49 +0800

    v1

---
+++ js/utils.js
@@ -0,0 +1,34 @@
+        function base64ToBlob(code) {
+            let parts = code.split(";base64,");
+            let contentType = parts[0].split(":")[1];
+            let raw = window.atob(parts[1]);
+            let rawLength = raw.length;
+            let uInt8Array = new Uint8Array(rawLength);
+            for (let i = 0; i < rawLength; ++i) {
+                uInt8Array[i] = raw.charCodeAt(i);
+            }
+            return new Blob([uInt8Array], {
+                type: contentType
+            });
+        }
+
+        function downloadFile(fileName, content) {
+            let aLink = document.createElement("a");
+            let blob = base64ToBlob(content);
+            console.log(blob);
+            let evt = document.createEvent("HTMLEvents");
+            evt.initEvent("click", true, true);
+            aLink.download = fileName;
+            aLink.href = URL.createObjectURL(blob);
+            aLink.click();
+        }
+
+        function file2base64(file) {
+            return new Promise((resolve, reject) => {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    resolve(e.target.result)
+                }
+                var base64 = reader.readAsDataURL(file);
+            })
+        }
